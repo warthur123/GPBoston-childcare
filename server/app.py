5a27b5791e79df1e44f3c86c9e87b78848e6e0b2
Arthur Wu <warthur123@gmail.com>
2022-01-27 00:01:15 -0500

    initilizae database link

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@ import psycopg2
 
 app = Flask(__name__)
 
-POSTGRESQL_URI = "postgres://ourpesjoilxxyk:88ed6d3d6aa0e7fe169cc5947d3cc7ba5a99a20c34603454c504b225e8c26e03@ec2-52-70-25-37.compute-1.amazonaws.com:5432/d814m5eo6dqo4c"
+POSTGRESQL_URI = ""
 
 try:
     conn = psycopg2.connect(POSTGRESQL_URI)
